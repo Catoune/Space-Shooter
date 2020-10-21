cf76c40335c23f3db3434b46819c0bb9f26c0b7d
Alexandre COURTIN <aoyama_masaya@hotmail.fr>
2020-10-21 23:05:59 +0200

    Continue

--- Workshop Unity/Assets/Scripts/Behaviors/PlayerController.cs
+++ Workshop Unity/Assets/Scripts/Behaviors/PlayerController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float x;
+    float y;
+
+    Vector3 axisX;
+    Vector3 axisY;
+
+    float speed = 6f;
+    bool justPressed = false;
+
+    public float delay = 0.5f;
+
+    private void Update()
     {
-        
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        axisX = (new Vector3(x, 0f, 0f)) * speed * Time.deltaTime;
+        axisY = (new Vector3(0f, y, 0f)) * speed * Time.deltaTime;
+
+        if ((y > 0 && transform.position.y <= 9f) || (y < 0 && transform.position.y >= 1f))
+        {
+            transform.position += axisY;
+        }
+        if((x > 0 && transform.position.x <= 9f) || (x < 0 && transform.position.x >= 1f))
+        {
+            transform.position += axisX;
+        }
+
+        if(Input.GetButton("Jump") && !justPressed)
+        {
+            justPressed = true;
+            ShootManager.instance.InstantiateShoot();
+            StartCoroutine("DelayShoot");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DelayShoot()
     {
-        
+        yield return new WaitForSeconds(delay);
+        justPressed = false;
     }
 }
---
+++ Workshop Unity/Assets/Scripts/Manager/ShootManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootManager : MonoBehaviour
+{
+    public static ShootManager instance;
+    public ShootBehaviour[] shootList;                //Recup dans l'éditeur
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public virtual void Init()
+    {
+        instance = this;
+    }
+
+    public void InstantiateShoot()
+    {
+        foreach(ShootBehaviour shoot in shootList)
+        {
+            if(!shoot.isUse)
+            {
+                shoot.isUse = true;
+                shoot.transform.position = PlayerManager.instance.canon.transform.position;
+                shoot.gameObject.active = true;
+                return;
+            }
+        }
+    }
+}
